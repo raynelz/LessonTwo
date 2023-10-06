aede26386bfb07003c83c0119a171e5a0e1ce382
Zahar Litvinchuk <97597336+raynelz@users.noreply.github.com>
2023-10-06 09:13:02 +0300

    final commit

--- LessonTwo/ViewController.swift
+++ LessonTwo/ViewController.swift
@@ -8,9 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+   
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var button: UIButton!
+    
+    var 🦄 = "unicorn"
+    
+    let cats = [
+        UIImage.gifImageWithName("1"),
+        UIImage.gifImageWithName("2"),
+        UIImage.gifImageWithName("3"),
+        UIImage.gifImageWithName("4"),
+        UIImage.gifImageWithName("5"),
+        UIImage.gifImageWithName("6"),
+        UIImage.gifImageWithName("7"),
+        UIImage.gifImageWithName("8"),
+        UIImage.gifImageWithName("9"),
+        UIImage.gifImageWithName("10"),
+        UIImage.gifImageWithName("11"),
+        UIImage.gifImageWithName("12")
+    ]
+    
+  
+
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imageView.image = UIImage.gifImageWithName("1")
+        
+        // Кто       . Что
+            //questionLabel.text = "TEST"
+        
+    }
+    
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        imageView.image = cats.randomElement()!
+        button.tintColor = .red
+        
+        // Кто.что = результат
+        
+        
     }
     
 }
